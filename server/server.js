e0b16eb39f68ce813e427d5c4411bdbb1d768276
lounis <lounis@teeps.co>
2020-02-27 17:54:27 +0100

    ajout d'une connection a MongoDB Atlas cluster

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Remplacer avec ton url mlab
-const MONGO_URI = 'mongodb://robinuser:robinuser@ds245347.mlab.com:45347/dbrobin001';
+/*const MONGO_URI = 'mongodb://robinuser:robinuser@ds245347.mlab.com:45347/dbrobin001';
 if (!MONGO_URI) {
   throw new Error('Tu dois fournir une url mongoDB');
 }
@@ -20,7 +20,16 @@ mongoose.connect(MONGO_URI,
 });
 mongoose.connection
     .once('open', () => console.log('Connecté à MongoLab'))
-    .on('error', error => console.log('Erreur de connexion à MongoLab:', error));
+    .on('error', error => console.log('Erreur de connexion à MongoLab:', error));*/
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://lounis:Denis-Paris7@cluster0-mipky.mongodb.net/dblounis?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("dblounis").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
